88614c3d7a1a127609cdb648c4eecee959455091
mhkhan4 <mhkhan4@buffalo.edu>
2022-07-03 16:47:26 -0400

    added spring initializer to the project

---
+++ src/main/java/com/brewery/kingbrewery/KingBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.brewery.kingbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brewery/kingbrewery/KingBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery.kingbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
